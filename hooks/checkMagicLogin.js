08360fc0dbc8265e75442256de40d2b80c6a3968
Henry Ugo <thehenryugo@gmail.com>
2023-03-10 03:18:36 -0600

    update: styled sidenav on dashboard

--- hooks/checkMagicLogin.js
+++ hooks/checkMagicLogin.js
@@ -1,26 +1,26 @@
-import { Magic } from 'magic-sdk';
-import { SolanaExtension } from '@magic-ext/solana';
-const web3 = require('@solana/web3.js');
+import { Magic } from "magic-sdk";
+import { SolanaExtension } from "@magic-ext/solana";
+const web3 = require("@solana/web3.js");
 
 export const checkMagicLogin = async () => {
-    const magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_KEY, {
-        extensions: {
-            solana: new SolanaExtension({
-                rpcUrl: process.env.NEXT_PUBLIC_SOLANA_RPC_URL,
-            }),
-        },
-    });
-    const loggedIn = await magic.user.isLoggedIn();
-    console.log('loggedIn', loggedIn)
-    if(loggedIn) {
-        magic.user.isLoggedIn().then(async (magicIsLoggedIn) => {
-            window.dispatchEvent(new Event('magic-logged-in'));
-        });
-        // get user data
-        const data = await magic.user.getMetadata();
-        localStorage.setItem('userMagicMetadata', JSON.stringify(data));
-        window.dispatchEvent(new Event('magic-logged-in'));
-        return true
-    }
-    return false
-}
+  // const magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_KEY, {
+  //     extensions: {
+  //         solana: new SolanaExtension({
+  //             rpcUrl: process.env.NEXT_PUBLIC_SOLANA_RPC_URL,
+  //         }),
+  //     },
+  // });
+  // const loggedIn = await magic.user.isLoggedIn();
+  // console.log('loggedIn', loggedIn)
+  // if(loggedIn) {
+  //     magic.user.isLoggedIn().then(async (magicIsLoggedIn) => {
+  //         window.dispatchEvent(new Event('magic-logged-in'));
+  //     });
+  //     // get user data
+  //     const data = await magic.user.getMetadata();
+  //     localStorage.setItem('userMagicMetadata', JSON.stringify(data));
+  //     window.dispatchEvent(new Event('magic-logged-in'));
+  //     return true
+  // }
+  return false;
+};
