a95a7d27cdb27fca1cd5e72bcb9c46068a27f666
Henry Ugo <thehenryugo@gmail.com>
2023-03-10 22:58:24 -0600

    update: styled login popup

--- hooks/checkMagicLogin.js
+++ hooks/checkMagicLogin.js
@@ -3,23 +3,23 @@ import { SolanaExtension } from "@magic-ext/solana";
 
 export const checkMagicLogin = async () => {
   const magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_KEY, {
-      extensions: {
-          solana: new SolanaExtension({
-              rpcUrl: process.env.NEXT_PUBLIC_SOLANA_RPC_URL,
-          }),
-      },
+    extensions: {
+      solana: new SolanaExtension({
+        rpcUrl: process.env.NEXT_PUBLIC_SOLANA_RPC_URL,
+      }),
+    },
   });
   const loggedIn = await magic.user.isLoggedIn();
-  console.log('loggedIn', loggedIn)
-  if(loggedIn) {
-      magic.user.isLoggedIn().then(async (magicIsLoggedIn) => {
-          window.dispatchEvent(new Event('magic-logged-in'));
-      });
-      // get user data
-      const data = await magic.user.getMetadata();
-      localStorage.setItem('userMagicMetadata', JSON.stringify(data));
-      window.dispatchEvent(new Event('magic-logged-in'));
-      return true
+  console.log("loggedIn", loggedIn);
+  if (loggedIn) {
+    magic.user.isLoggedIn().then(async (magicIsLoggedIn) => {
+      window.dispatchEvent(new Event("magic-logged-in"));
+    });
+    // get user data
+    const data = await magic.user.getMetadata();
+    localStorage.setItem("userMagicMetadata", JSON.stringify(data));
+    window.dispatchEvent(new Event("magic-logged-in"));
+    return true;
   }
   return false;
 };
